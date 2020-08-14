bb838bfe248b0485d7f621c827d333b1134a1af9
Hector Frohlich Cortez <frohlichcortezh@protonmail.com>
2020-08-14 14:25:06 +0200

    End of Angular Tutorial 5 - Interpolation

--- binding/src/app/test/test.component.ts
+++ binding/src/app/test/test.component.ts
@@ -2,14 +2,30 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  template: `
+    <h2>
+      Welcome {{name}}
+    </h2>
+    <h2>{{2+2}}</h2>
+    <h2>{{"Welcome " + name}}</h2>
+    <h2>{{name.length}}</h2>
+    <h2>{{name.toUpperCase()}}</h2>
+    <h2>{{greetUser()}}</h2>
+    <h2>{{siteurl}}</h2>`,
+  styles: []
 })
 export class TestComponent implements OnInit {
+
+  public name = "Vishwas";
+  public siteurl = window.location.href;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  greetUser() {
+    return "Hello " + this.name;
   }
 
 }
