05c8509598cf26942667afd58bf386f869cb01e3
Hector Frohlich Cortez <frohlichcortezh@protonmail.com>
2020-08-18 22:44:46 +0200

    End of Angular Tutorial - 7 - Class Binding

--- binding/src/app/test/test.component.ts
+++ binding/src/app/test/test.component.ts
@@ -3,22 +3,52 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-    <h2>
+    <h2 class="text-success">
       Welcome {{name}}
     </h2>
+    <h2 [class]="successClass">
+      Codevolution
+    </h2>
+    <h2 class="text-special" [class]="successClass">
+      Codevolution
+    </h2>  
+    <h2 [class.text-danger]="hasError">
+      Codevolution
+    </h2>    
+    <h2 [ngClass]="messageClasses">
+      Codevolution
+    </h2>    
     <input [id]="myId" type="text" value="Vishwas">
     <input [disabled]="isDisabled" id={{myId}} type="text" value="Vishwas">
     <input bind-disabled="isDisabled" id={{myId}} type="text" value="Vishwas">
   
   `,
-  styles: []
+  styles: [`
+    .text-success {
+      color: green;      
+    }
+    .text-danger {
+      color: red;
+    }
+    .text-special {
+      font-style: italic;
+    }
+  `]
 })
 export class TestComponent implements OnInit {
 
   public name = "Vishwas";
   public myId = "testId";
   public isDisabled = true;
+  public hasError = false;
+  public successClass = "text-success";
 
+  public isSpecial = true;
+  public messageClasses = {
+    "text-success": !this.hasError,
+    "text-danger": this.hasError,
+    "text-special": this.isSpecial
+  }
   constructor() { }
 
   ngOnInit(): void {
